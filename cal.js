39c837575bb63540dfb7f55c8772e3c195826128
Abhishek Tomar <61470005+Thakur-15x@users.noreply.github.com>
2021-09-07 20:25:09 +0530

    initial commit

---
+++ cal.js
@@ -0,0 +1,21 @@
+let screen= document.getElementById("screen");
+button= document.querySelectorAll("button");
+let Value= ''
+for (item of button){
+    item.addEventListener('click',(e)=>{
+        buttonText= e.target.innerText
+        // console.log(buttonText)
+        if(buttonText=='C'){
+            screen.value = Value;
+            Value = '';
+        }
+        else if(buttonText=='='){
+            screen.value= eval(Value);
+        }
+        else{
+            Value += buttonText;
+            screen.value = Value;
+        }
+    })
+
+}
